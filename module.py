70a2ce074a02c5f10a461c42765973e113233df9
constuelo <ferguson.rory@gmail.com>
2019-01-03 16:18:19 +0000

    Update

--- module.py
+++ module.py
@@ -3,14 +3,15 @@ import os
 import json
 from pathlib import Path
 from collections import namedtuple
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 import re
+from html.parser import HTMLParser
 
 """ Export image from a photoshop file """
 psd = 'test.psd'
 psd_load = PSDImage.load(Path(os.path.dirname(__file__)) / psd)
 
-module_list_from_psd = []
+module_list_from_psd  = []
 
 
 def get_module_names_content(container):
@@ -21,7 +22,6 @@ def get_module_names_content(container):
                 """ ['TEXT_02'] """
                 name = [layer.name]
                 module_list_from_psd.append(name)
-
                 """ Module contents"""
                 """ ['TEXT_02', ['HEADER THREE', 'FuturaPT-Demi', '20px', '#1a1a1a', '0.08em']] """
                 recurse(layer, name=layer.name, module=name)
@@ -43,7 +43,8 @@ def recurse(container, name, module):
                 if layer.kind == 'type':
 
                     """ font_type """
-                    font_type = layer.text.rstrip().replace('\r', '\n')
+                    # font_type = layer.text.rstrip().replace('\r', '\n').rstrip()
+                    font_type = layer.text.rstrip().replace('\r', '<br class="d_d_b" />')
 
                     """ font_type using EngineData """
                     # print(str(layer.engine_data[b'EngineDict'][b'Editor'][b'Text']).rstrip().replace('\r', '\n'))
@@ -91,22 +92,61 @@ def get_module_html(name):
 
 def replace(name):
     html = get_module_html(name[0])
-    font_type = name[1][0]
     soup = BeautifulSoup(html, 'html.parser')
-    emails = soup.find_all('td')
-    print(emails)
+    tags = []
+    for a in soup.findAll('a'):
+        if not a.find('img'):
+            tags.append(a.text)
+    go = 0
+
+    encoding_dict = {
+        'Ä': '&Auml;', 'ä': '&auml;', 'É': '&Eacute;',
+        'é': '&eacute;', 'Ö': '&Ouml;', 'ö': '&ouml;',
+        'Ü': '&Uuml;', 'ü': '&uuml;', 'ß': '&szlig;',
+        '‘': '&lsquo;', '’': '&rsquo;', '“': '&ldquo;',
+        '”': '&rdquo;', '€': '&euro;', '£': '&pound;',
+        '…': '...'
+    }
+
+    encoded_tags = []
+    for i in tags:
+        a = [str(char).replace(char, encoding_dict.get(char, char)) for char in i]
+        a = "".join(a)
+        encoded_tags.append(a)
+    print(encoded_tags)
+
+    if len(encoded_tags) == len(name) - 1:
+        for i in encoded_tags:
+            go += 1
+            # print(i, name[go][0])
+            html = html.replace(i, name[go][0])
+    # print(html)
+
+    return html
+
+
+def write_out(html_list):
+    data = html_list
+    counter = 4
+    with open('modules.htm', 'w') as f:
+        for v in data:
+            counter += 1
+            f.write(f'\t\t\t<div data-content-region-name="region_0{counter}">\n')
+            f.write(v)
+            f.write('\n\t\t\t</div>\n\n')
+
+    f.close()
 
 
 for i in psd_load.layers:
     if 'MOBILE'.lower() in i.name.lower():
         """ Get module names from psd """
         modules = get_module_names_content(i)
-        # for i in modules:
-        #     print(i[0])
 
+        html_list = []
         """ Get module html from modules.json """
         for mod in modules:
-            # get_module_html(mod[0])
-            # print(get_module_html(mod[0]))
-            """ replace """
-            replace(mod)
+            """ replace text in html """
+            html_list.append(replace(mod))
+            """ write out to file """
+            write_out(html_list=html_list)
