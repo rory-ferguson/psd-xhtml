b3c0a6b94f0e1c16d8a37ca14307026ac03ff664
constuelo <ferguson.rory@gmail.com>
2019-07-15 14:41:20 +0100

    working before dict change

--- main_v2.py
+++ main_v2.py
@@ -6,8 +6,6 @@ from bs4 import BeautifulSoup
 from pathlib import Path
 from colorama import Fore, Style
 
-module_list_from_psd = []
-
 root = os.path.dirname(__file__)
 
 # user_directory = input('PSD path:')
@@ -33,42 +31,58 @@ if not psd:
 if not path_of_psd:
     path_of_psd = Path(user_directory).joinpath(psd)
 
-print(f'\nLoading {Fore.BLUE}{psd}{Style.RESET_ALL}')
+# print(f'\nLoading {Fore.BLUE}{psd}{Style.RESET_ALL}')
 psd_load = PSDImage.open(path_of_psd)
-print(f'Finished loading {Fore.BLUE}{psd}{Style.RESET_ALL}\n')
+# print(f'Finished loading {Fore.BLUE}{psd}{Style.RESET_ALL}\n')
 
 
-def get_module_names_content(container):
+def module_list(layer):
+    """ Return module list from the psd file with text
+        :param: layer
+        :ptype: psd_tools.api.layers.Group
+        :return: modules
+        :rtype: list
+    """
     try:
-        for layer in reversed(list(container)):
-            if layer.is_visible() and layer.kind == 'group':
-                """ Module list names """
-                """ ['TEXT_02'] """
-                name = [layer.name]
-                module_list_from_psd.append(name)
-                """ Module contents"""
-                """ ['TEXT_02', ['HEADER THREE', 'FuturaPT-Demi', '20px', '#1a1a1a', '0.08em']] """
-                recurse(layer, m=name)
-        print(module_list_from_psd)
-        return module_list_from_psd
+        if layer.is_visible() and layer.kind == 'group':
+            """ Module list names """
+            name = [layer.name]
+            modules.append(name)
+            extract_module_text(layer, module=name)
+
+        return modules
 
     except AttributeError:
         pass
 
+def colour_to_hex(font_color):
+    t = []
+    for index, item in enumerate(font_color[1:4]):
+        t.append((int(round(item * 255))))
+    return '#%02x%02x%02x' % (t[0], t[1], t[2])
 
-def recurse(container, m):
-    """ Recursive loop over each layer to extract all the text
+def extract_module_text(container, module):
+    """ Extract the module text from the psd file
+        example: ['TEXT_02', ['HEADER THREE', 'FuturaPT-Demi', '20px', '#1a1a1a', '0.08em']]
+        :param: container, module
+        :ptype: psd_tools.api.layers.Group, list
+        :return: module
+        :rtype: list
     """
-    print(container, m)
     try:
         for layer in reversed(list(container.descendants())):
-            if layer.is_visible():
-                if layer.kind == 'type':
-                    """ font_type """
-                    font_type = layer.text.rstrip().replace('\r', ' ')
+            if layer.is_visible() and layer.kind == 'type':
+                """ font_type """
+                style_sheet = layer.engine_dict['StyleRun']['RunArray'][0]
 
-                    lst = [font_type]
-                    m.append(lst)
+                font_type = layer.text.rstrip().replace('\r', ' ')
+                font_size = int(round(style_sheet['StyleSheet']['StyleSheetData']['FontSize'] / 2))
+                font_tracking = f"{style_sheet['StyleSheet']['StyleSheetData']['Tracking'] / 1000:.2f}em"  # font-tracking
+                font_color = style_sheet['StyleSheet']['StyleSheetData']['FillColor']['Values']  # font-colour
+                font_color = colour_to_hex(font_color)  # convert colour to hex code
+                type_data = [font_type], [layer.name], [font_size]
+                module.append(type_data)
+        return module
 
     except AttributeError:
         pass
@@ -112,26 +126,6 @@ def encode(a):
     return b
 
 
-def no_blank(a):
-    """ this feature needs work and is not active
-        Adds &nbsp; before the last word of each text string to prevent single words on one line
-    """
-    try:
-        b = a.rsplit(' ', 1)
-
-        if b[1]:
-            b[1] = f'&nbsp;{b[1]}'
-            b = ''.join(b)
-
-        if not b[1]:
-            b = ''.join(b)
-
-    except IndexError:
-        pass
-
-    return b
-
-
 def replace(name):
     html = get_module_html(name[0])
     soup = BeautifulSoup(html, 'html.parser')
@@ -147,15 +141,22 @@ def replace(name):
         for m in encode_module_text:
             go += 1
             a = encode(name[go][0])
-            """ this feature needs work and is not active
-            no_blank(a[0])
-            """
             html = html.replace(m, a[0], 1)
     else:
         print(f'{Fore.RED}ALERT! {name[0]} module has not been updated.{Style.RESET_ALL} '
               f'There are {len(encode_module_text)} html and {len(name) - 1} psd text containers.\n')
 
     return html
+
+
+def some_func(module):
+    resources = Path(root) / 'resources'
+    text_template = Path(resources) / 'TEXT_TEMPLATE.htm'
+    with open(f'{text_template}') as f:
+        template = f.read()
+    name = module[1][1][0]
+    font_text = module[1][0][0]
+    font_size = module[1][2][0]
 
 
 def write_out(lst):
@@ -183,28 +184,36 @@ def write_out(lst):
 
 print(f'The file {Fore.BLUE}{psd}{Style.RESET_ALL} is being parsed.\n')
 
-for i in psd_load:
-    if 'MOBILE'.lower() in i.name.lower():
-        mobileArtboard = i
-        """ Get module names from psd """
-        modules = get_module_names_content(i)
-        html_lst = []
-        """ Get module html from modules.json """
-        for mod in modules:
-            print(f'{Fore.BLUE}{mod[0]}{Style.RESET_ALL}')
-            """ replace text in html """
-            try:
-                html_lst.append(replace(mod))
-            except TypeError:
-                pass
+for artboard in psd_load:
+    if 'MOBILE'.lower() in artboard.name.lower() and artboard.kind == 'artboard':
+        mobile_artboard = artboard
+        modules = []
 
+        for layer in reversed(list(artboard)):
+            modules = module_list(layer)
+        print(modules)
+
+        """ Get module html from modules.json """
+        html_lst = []
+        for mod in modules:
+            # print(f'{Fore.BLUE}{mod[0]}{Style.RESET_ALL}')
+
+            if mod[0] == 'DYNAMIC_TEXT':
+                some_func(mod)
+                pass
+            else:
+                """ replace text in html """
+                try:
+                    html_lst.append(replace(mod))
+                except TypeError:
+                    pass
 
             """ write out to file """
             write_out(lst=html_lst)
 
 
-if mobileArtboard is None:
-    print('There was a problem.')
-    print('Please ensure the artboard names include one of the below')
-    print('_Desktop or _Mobile')
-    sys.exit()
+# if mobile_artboard is None:
+#     print('There was a problem.')
+#     print('Please ensure the artboard names include one of the below')
+#     print('_Desktop or _Mobile')
+#     sys.exit()
